e304d78dcd90533cd8ae4fcd7c5eec0c944d3f3c
Xavier Hernandez <xhernan.martinez@gmail.com>
2024-10-12 18:07:56 +0200

    chores: install antfu/eslint config and formatting

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <h3 class="text-center font-semibold text-green-500">Test TailwindCSS</h3>
+    <h3 class="text-center font-semibold text-green-500">
+      Test TailwindCSS
+    </h3>
     <NuxtWelcome />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// eslint-disable-next-line no-undef
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts'],
 })
